1f3acf9857f06e513940ff96f0da52f967fae06f
fishears <f1she4rs@gmail.com>
2012-11-05 15:09:58 +0000

    dalvik optimization: v=a,o=v enables verification, and only optimizes classes that successfully verified. This is the safest setting, and is the default in AOSP Android. u=y forces all classes to be optimised specifically for uniprocessors/single core CPUs. m=y registers maps.

--- device.mk
+++ device.mk
@@ -48,6 +48,10 @@ PRODUCT_COPY_FILES += \
 # We have FFC
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml
+	
+# Custom Dalvik flags
+PRODUCT_PROPERTY_OVERRIDES += \
+	dalvik.vm.dexopt-flags=v=a,o=v,u=y,m=y
 
 # Inherit Aries common device configuration.
 $(call inherit-product, device/samsung/aries-common/device_base.mk)
